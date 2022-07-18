f2bfcbea8e37979bd058c9012f4a1e8a6a917afa
CabreroBR <caio.porta12@gmail.com>
2022-07-18 18:52:19 -0300

    First Class

---
+++ Aula 1/Projeto 1/Imersão JAVA - Projeto 1 - Alura Stickers/src/App.java
@@ -0,0 +1,34 @@
+import java.net.URI;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        // fazer uma conexão http e buscar os top 250 filmes
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_beojdf0c";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // filtrar/parsear os dados úteis (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        //System.out.println(listaDeFilmes.size());
+
+        // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
+    }
+}
